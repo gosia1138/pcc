50edcd74e64422cae07952074504d0b7486c108a
mmr <77846862+gosia1138@users.noreply.github.com>
2021-11-23 14:11:02 +0100

    Rewrite script for new main function
    Script to focus on grouping images from a directory based on time or geographical data.

--- imagine.py
+++ imagine.py
@@ -7,113 +7,73 @@ from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 
 
-def create_source_list(dir):
-    '''Returning list of dictionaries representing images contained in the directory'''
+class ImageMeta():
+    def __init__(self, file_path, file_name, file_extension):
+        self.path = file_path
+        self.name = file_name
+        self.type = file_extension
+        img_data = self.extract_data()
+        self.exif = img_data[0]
+        self.size = img_data[1]
+        self.datetime = self.get_datetime()
+
+    def extract_data(self):
+        '''extracting image exif and size data'''
+        exif_data = {}
+        with Image.open(self.path) as im:
+            info = im._getexif()
+            size = im.size
+        if info:
+            for tag, value in info.items():  # decoding exif data
+                decoded = TAGS.get(tag, tag)
+                if decoded == "GPSInfo":
+                    gps_data = {}  # creating a sub-dictionary of gps info
+                    for t in value:
+                        sub_decoded = GPSTAGS.get(t, t)
+                        gps_data[sub_decoded] = value[t]
+                    exif_data[decoded] = gps_data
+                else:
+                    exif_data[decoded] = value
+        return exif_data, size
+
+    def get_datetime(self):
+        datetime_str = self.exif.get("DateTime", None)
+        if datetime_str:
+            pattern = r"(\d\d\d\d):(\d\d):(\d\d) (\d\d):(\d\d):(\d\d)"
+            datetime = re.search(pattern, datetime_str)
+            return datetime
+        return None
+
+
+def create_source_list(dir, valid_extensions):
+    '''Returning list of tuples representing images contained in the directory'''
     if not os.path.isdir(dir):
         sys.exit("Entered directory is not valid!")
     source = []
     files = os.listdir(dir)
-    accepted_extensions = ('jpg', 'jpeg')
-    for file in files:
-        file_path = os.path.join(dir, file)
+    for file_name in files:
+        file_path = os.path.join(dir, file_name)
+        file_dir, file_extension = os.path.splitext(file_path)
         if os.path.isdir(file_path):
             continue    # skip directories
-        if not file.lower().endswith(accepted_extensions):
+        if file_extension.lower() not in valid_extensions:
             continue    # skip files that do not have accepted extensions
-        source.append({
-                'file': file,
-                'full_path': file_path,
-                'dir': dir,
-            })
+        source.append((file_path, file_name, file_extension))
     return source
-
-def extract_exif(im):
-    exif_data = {}
-    info = im._getexif()
-    if info:
-        for tag, value in info.items():
-            decoded = TAGS.get(tag, tag)
-            if decoded == "GPSInfo":
-                gps_data = {}
-                for t in value:
-                    sub_decoded = GPSTAGS.get(t, t)
-                    gps_data[sub_decoded] = value[t]
-                exif_data[decoded] = gps_data
-            else:
-                exif_data[decoded] = value
-    return exif_data
-
-
-def check_valid_filename(filename):
-    '''Checking validity of constructed filenames'''
-    valid_pattern = re.match(r'^[a-zA-Z0-9_-]+$', filename)
-    valid_length = len(filename) < 256
-    if valid_pattern and valid_length:
-        return True
-    return False
-
-
-def resize_images(source, max_size):
-    '''getting paths of chosen directory, opening it one by one and resizing'''
-    size = max_size, max_size
-    thumbnails_dir = create_new_subdir(source[0]['dir'], "{}px_".format(str(max_size)))
-    for image in source:
-        with Image.open(image['full_path']) as im:
-            if im.size[0] > max_size or im.size[1] > max_size:
-                im.thumbnail(size)
-            print("{:<20} - {} x {} pixels".format(image['file'], im.size[0], im.size[1]))
-            exif_dict = extract_exif(im)
-            os.chdir(thumbnails_dir)
-            im.save(image['file'], "JPEG", exif=im.getexif())
-
-
-def rename_images(source, name_pattern):
-    '''renaming images with proposed pattern followed by incrementing numbers'''
-    renamed_files_dir = create_new_subdir(source[0]['dir'], name_pattern)
-    file_count = 0
-    for image in source:
-        filename, file_extension = os.path.splitext(image['file'])
-        new_name = name_pattern + str(file_count) + file_extension
-        destination = os.path.join(renamed_files_dir, new_name)
-        shutil.copy2(image['full_path'], destination)
-        file_count += 1
-
-
-def create_new_subdir(dir, name):
-    '''Creating subdirectory inside images directory to store resized images'''
-    subdir_count = 0
-    while os.path.isdir(os.path.join(dir, "{}{}".format(name, str(subdir_count)))):
-        subdir_count += 1
-    path = os.path.join(dir, "{}{}".format(name, str(subdir_count)))
-    os.mkdir(path)
-    return path
 
 
 def main():
-    # TEMPORARY CLI
-    '''Checking for command line arguments to pass to resizing function'''
-    if len(sys.argv) == 4:
-        source_dir = create_source_list(sys.argv[2])
-        # ------RESIZE------
-        if sys.argv[1] in ('-R', '--resize'):
-            max_size = sys.argv[3]
-            if not max_size.isdigit():
-                sys.exit("Size parameter must be an integer!")
-            resize_images(source_dir, int(max_size))
-        # ------RENAME------
-        elif sys.argv[1] in ('-r', '--rename'):
-            new_name = sys.argv[3]
-            rename_images(source_dir, new_name)
+    valid_extensions = ('.jpg', '.jpeg')
+    if len(sys.argv) == 2:
+        img_dir = sys.argv[1]
+        source_list = create_source_list(img_dir, valid_extensions)
+        images = [ImageMeta(*file_data) for file_data in source_list]
 
+        for img in images:
+            print(img.name)
+            print(img.get_datetime())
     else:
-        sys.exit('''
-To use command line input use following pattern:
-
-FOR RESIZING:
-$ ./imagine --resize <images directory> <maximum size>
-
-FOR RENAMING:
-$ ./imagine --rename <images directory> <name pattern>''')
+        sys.exit("""Correct input: 'python3 imagine.py <images_directory>'""")
 
 
 if __name__ == '__main__':
--- test/test.py
+++ test/test.py
@@ -1,42 +1,13 @@
 #!/usr/bin/env python3
-import PIL
-import os
 import unittest
-from io import StringIO
-from unittest.mock import patch
 from .context import *
 
 
 class TestValidFilename(unittest.TestCase):
 
     def test_correct_name(self):
-        '''Should return true when fed correct filename'''
-        name = 'good-file_name1138'
-        result = check_valid_filename(name)
-        self.assertTrue(result)
+        pass
 
-    def test_prohibited_names(self):
-        '''Should return false when fed bad filenames'''
-        names = [
-            'you.can.but.you.should.not',
-            'spaces are not cool',
-            '@b$olut&ly*not?!',
-            '/justdont',
-        ]
-        result = [not check_valid_filename(filename) for filename in names]
-        self.assertTrue(all(result))
-
-    def test_empty_string(self):
-        '''Empty string should return false'''
-        name = ''
-        result = check_valid_filename(name)
-        self.assertFalse(result)
-
-    def test_too_long_name(self):
-        '''name over 255 should return false'''
-        name = 'a' * 256
-        result = check_valid_filename(name)
-        self.assertFalse(result)
 
 if __name__ == '__main__':
     unittest.main()
