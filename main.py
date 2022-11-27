e986a76fdd2b3090af007c88a41a83cad3132db4
mrutkowska <77846862+gosia1138@users.noreply.github.com>
2022-11-27 15:00:56 +0100

    move ImageMeta to separate file

--- main.py
+++ main.py
@@ -5,79 +5,8 @@ import sys
 from datetime import datetime, timedelta
 from math import acos, sin, cos, radians
 from operator import attrgetter
-from PIL import Image
-from PIL.ExifTags import TAGS, GPSTAGS
-from shutil import copyfile
 from pcc.directory import Directory, GetJPGFilesStrategy
-
-
-class ImageMeta():
-    def __init__(self, file_path, file_dir, file_name, file_extension):
-        '''extract file info from path and collect other data from exif'''
-        # image data
-        self.path = file_path
-        self.dir = file_dir
-        self.name = file_name
-        self.type = file_extension
-        self.extract_data()
-        self.get_datetime()
-        if not getattr(self, "coords", None):
-            self.place = "unknown"
-        # grouping data
-        self.grouping_factors = []
-
-    def extract_data(self):
-        '''extracting image exif and size data'''
-        exif_data = {}
-        with Image.open(self.path) as im:
-            info = im._getexif()
-            self.size = im.size
-        if info:
-            for tag, value in info.items():  # decoding exif data
-                decoded = TAGS.get(tag, tag)
-                if decoded == "GPSInfo":
-                    gps_data = {}  # creating a sub-dictionary of gps info
-                    for t in value:
-                        sub_decoded = GPSTAGS.get(t, t)
-                        gps_data[sub_decoded] = value[t]
-                    exif_data[decoded] = gps_data
-                    if gps_data:  # creating attribute "coords" from EXIF GPS data
-                        lat_d, lat_m, lat_s = [int(value) for value in gps_data["GPSLatitude"]]
-                        lat_sign = gps_data["GPSLatitudeRef"]
-                        lat = (lat_d + lat_m/60 + lat_s/3600) * (1 - 2*(lat_sign == "S"))
-                        long_d, long_m, long_s = [int(value) for value in gps_data["GPSLongitude"]]
-                        long_sign = gps_data["GPSLongitudeRef"]
-                        long = (long_d + long_m/60 + long_s/3600) * (1 - 2*(long_sign == "W"))
-                        self.coords = (lat, long)
-                else:
-                    exif_data[decoded] = value
-        self.exif = exif_data
-
-    def get_datetime(self):
-        '''Try to get creation date from exif or fallback to ctime. Return a datetime object'''
-        created = datetime.fromtimestamp(os.path.getctime(self.path))
-        datetime_str = self.exif.get("DateTime", None)
-        if datetime_str:
-            created = datetime.strptime(datetime_str, "%Y:%m:%d %H:%M:%S")
-        self.created = created
-
-    def grouping_dir(self, sub_dir):
-        '''returns a group subdirectory to which image should be copied'''
-        grouping_dir = os.path.join(self.dir, sub_dir)
-        for factor in self.grouping_factors:
-            if factor == "unknown":
-                break
-            grouping_dir = os.path.join(grouping_dir, str(factor))
-        return grouping_dir, self.name
-
-    def make_copy(self, sub_dir):
-        '''Copy image into provided directory'''
-        destination_dir, file_name = self.grouping_dir(sub_dir)
-        if not os.path.isdir(destination_dir):
-            os.makedirs(destination_dir)
-        destination = os.path.join(destination_dir, file_name)
-        if self.path != destination:
-            copyfile(self.path, destination)
+from pcc.images import ImageMeta
 
 
 class Place():
---
+++ pcc/images.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+import os
+from shutil import copyfile
+
+
+class ImageMeta():
+    def __init__(self, full_path, directory, filename, extension):
+        '''extract file info from path and collect other data from exif'''
+        # image data
+        self.path = full_path
+        self.dir = directory
+        self.name = filename
+        self.type = extension
+        self.extract_data()
+        self.get_datetime()
+        if not getattr(self, "coords", None):
+            self.place = "unknown"
+        # grouping data
+        self.grouping_factors = []
+
+    def extract_data(self):
+        '''extracting image exif and size data'''
+        exif_data = {}
+        with Image.open(self.path) as im:
+            info = im._getexif()
+            self.size = im.size
+        if info:
+            for tag, value in info.items():  # decoding exif data
+                decoded = TAGS.get(tag, tag)
+                if decoded == "GPSInfo":
+                    gps_data = {}  # creating a sub-dictionary of gps info
+                    for t in value:
+                        sub_decoded = GPSTAGS.get(t, t)
+                        gps_data[sub_decoded] = value[t]
+                    exif_data[decoded] = gps_data
+                    if gps_data:  # creating attribute "coords" from EXIF GPS data
+                        lat_d, lat_m, lat_s = [int(value) for value in gps_data["GPSLatitude"]]
+                        lat_sign = gps_data["GPSLatitudeRef"]
+                        lat = (lat_d + lat_m/60 + lat_s/3600) * (1 - 2*(lat_sign == "S"))
+                        long_d, long_m, long_s = [int(value) for value in gps_data["GPSLongitude"]]
+                        long_sign = gps_data["GPSLongitudeRef"]
+                        long = (long_d + long_m/60 + long_s/3600) * (1 - 2*(long_sign == "W"))
+                        self.coords = (lat, long)
+                else:
+                    exif_data[decoded] = value
+        self.exif = exif_data
+
+    def get_datetime(self):
+        '''Try to get creation date from exif or fallback to ctime. Return a datetime object'''
+        created = datetime.fromtimestamp(os.path.getctime(self.path))
+        datetime_str = self.exif.get("DateTime", None)
+        if datetime_str:
+            created = datetime.strptime(datetime_str, "%Y:%m:%d %H:%M:%S")
+        self.created = created
+
+    def grouping_dir(self, sub_dir):
+        '''returns a group subdirectory to which image should be copied'''
+        grouping_dir = os.path.join(self.dir, sub_dir)
+        for factor in self.grouping_factors:
+            if factor == "unknown":
+                break
+            grouping_dir = os.path.join(grouping_dir, str(factor))
+        return grouping_dir, self.name
+
+    def make_copy(self, sub_dir):
+        '''Copy image into provided directory'''
+        destination_dir, file_name = self.grouping_dir(sub_dir)
+        if not os.path.isdir(destination_dir):
+            os.makedirs(destination_dir)
+        destination = os.path.join(destination_dir, file_name)
+        if self.path != destination:
+            copyfile(self.path, destination)
