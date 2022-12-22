900ce14165af5f0e05efb2def014c2dc10a67889
mrutkowska <77846862+gosia1138@users.noreply.github.com>
2022-12-22 23:01:45 +0100

    refactor - argparse

--- main.py
+++ main.py
@@ -1,53 +1,46 @@
-#!/usr/bin/env python3
-
 import sys
-from datetime import datetime, timedelta
-from math import acos, sin, cos, radians
-from operator import attrgetter
 from pcc.directory import Directory, JPGFilterStrategy
-from pcc.file_objects import JPGFile
 from pcc.utils.grouping_strategies import (
     YearGroupingStrategy,
     MonthGroupingStrategy,
     YearMonthGroupingStrategy,
     PlaceGroupingStrategy,
     TimePlaceGroupingStrategy)
+import argparse
+from pathlib import Path
+
+
+grouping_actions_strategies = {
+        1: YearGroupingStrategy,
+        2: MonthGroupingStrategy,
+        3: YearMonthGroupingStrategy,
+        4: PlaceGroupingStrategy,
+        5: TimePlaceGroupingStrategy
+    }
 
 
 if __name__ == '__main__':
-    # create list of JPGFile classes from given directory
-    if len(sys.argv) == 2:
-        directory = sys.argv[1]
-        images_directory = Directory(directory, JPGFilterStrategy)
-    else:
-        sys.exit("Correct input: 'python3 imagine.py <images_directory>'")
+    parser = argparse.ArgumentParser(
+        prog="PCC - Photo Chaos Controller",
+        usage='main.py <directory> <options>',
+        description="Program to sort photos from your mobile phone or camera by means of date or/and place.")
+    parser.add_argument("directory", help="Directory to images you want to group", type=Path)
+    grouping_factor = parser.add_mutually_exclusive_group(required=True)
+    grouping_factor.add_argument("-y", "--year", help="Group images in given directory by year", dest='grouping_factors', action='append_const', const=1)
+    grouping_factor.add_argument("-m", "--month", help="Group images in given directory by month", dest='grouping_factors', action='append_const', const=2)
+    grouping_factor.add_argument("-d", "--date", help="Group images in given directory by year and then month", dest='grouping_factors', action='append_const', const=3)
+    grouping_factor.add_argument("-p", "--place", help="Group images in given directory by place", dest='grouping_factors', action='append_const', const=4)
+    grouping_factor.add_argument("-s", "--smart", help="Smart Grouping (place and date)", dest='grouping_factors', action='append_const', const=5)
+    args = parser.parse_args()
 
-    # prompt user about action to be taken with the images
-    actions = {
-        "1": YearGroupingStrategy,
-        "2": MonthGroupingStrategy,
-        "3": YearMonthGroupingStrategy,
-        "4": PlaceGroupingStrategy,
-        "5": TimePlaceGroupingStrategy
-    }
+    args_grouping_factor_index = args.grouping_factors[0]
+    args_directory = args.directory
 
-    print(f"\nThere are {images_directory.get_number_of_files()} images in {images_directory.directory}")
-    print("Here is what you can do with them:")
-    print("[1] Group by year")
-    print("[2] Group by month")
-    print("[3] Group by year and month")
-    print("[4] Group by places")
-    print("[5] Smart Grouping (place and date)")
-    print("[q] Quit")
-    
-    while True:
-        users_choice = input("> ")
-        if users_choice in actions.keys():
-            images_directory.set_grouping_strategy(actions[users_choice])
-            images_directory.add_grouping_factors_to_files()
-            images_directory.copy_files()
-            sys.exit()
-        elif users_choice.lower() == "q":
-            sys.exit("Goodbye!")
-        else:
-            print("Invalid input, try again.")
+    directory = Directory(args_directory, JPGFilterStrategy)
+
+    grouping_strategy = grouping_actions_strategies.get(args_grouping_factor_index)
+    directory.set_grouping_strategy(grouping_strategy)
+    directory.add_grouping_factors_to_files()
+    directory.copy_files()
+
+    print(f"\n{directory.get_number_of_files()} images from {directory.directory} processed successfully!")
