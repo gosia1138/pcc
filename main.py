9031aed06f9cd2cbb01efbba17f0d2c14f57b66c
mrutkowska <77846862+gosia1138@users.noreply.github.com>
2022-12-29 21:37:13 +0100

    refactor cli

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ grouping_actions_strategies = {
 
 
 if __name__ == '__main__':
+    grouping_strategies = []
     parser = argparse.ArgumentParser(
         prog="PCC - Photo Chaos Controller",
-        usage='main.py <directory> <options>',
-        description="Program to sort photos from your mobile phone or camera by means of date or/and place.")
+        usage='main.py <directory> <grouping factors>',
+        description='''Program to sort photos by chosen factors. You can choose one or more grouping factors,
+        they will be applied in the same order as you put them in the prompt.'''
+        )
     parser.add_argument("directory", help="Directory to images you want to group", type=Path)
     grouping_factor = parser.add_argument_group()
     grouping_factor.add_argument("-y", "--year", help="Group images in given directory by year", dest='grouping_factors', action='append_const', const=1)
@@ -31,10 +34,12 @@ if __name__ == '__main__':
     grouping_factor.add_argument("-d", "--date", help="Group images in given directory by year and then month", dest='grouping_factors', action='append_const', const=3)
     grouping_factor.add_argument("-p", "--place", help="Group images in given directory by place", dest='grouping_factors', action='append_const', const=4)
     grouping_factor.add_argument("-s", "--smart", help="Smart Grouping (place and date)", dest='grouping_factors', action='append_const', const=5)
+    
     args = parser.parse_args()
-
     args_grouping_factors = args.grouping_factors
     args_directory = args.directory
+    if not args_grouping_factors or not args_directory:
+        parser.error('No any grouping factors provided - please provide at least one')
 
     directory = Directory(args_directory, JPGFilterStrategy)
     grouping_strategies = list(map(lambda x: grouping_actions_strategies.get(x), args_grouping_factors))
